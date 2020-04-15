eee59897f54bf5668b96d50d3a259c0c5677c21f
Mohammad Ahmady Yeganeh <m.yegane49@outlook.com>
2020-04-15 13:25:27 +0430

    Add pages, layouts and components

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,34 @@
+<template>
+    <section class="post-list">
+        <PostPreview id="1" thumbnail="https://www.pixelstalk.net/wp-content/uploads/2016/06/HD-High-Tech-Wallpaper.jpg" title="Hello there!" previewText="This is my first post!" :is-admin="isAdmin" />
+        <PostPreview id="2" thumbnail="https://www.pixelstalk.net/wp-content/uploads/2016/06/HD-High-Tech-Wallpaper.jpg" title="Hello there - second!" previewText="This is my second post!" :is-admin="isAdmin" />
+        <PostPreview id="3" thumbnail="https://www.pixelstalk.net/wp-content/uploads/2016/06/HD-High-Tech-Wallpaper.jpg" title="Hello there - third!" previewText="This is my third post!" :is-admin="isAdmin" />
+    </section>
+</template>
+
+<script>
+    import PostPreview from '@/components/Posts/PostPreview.vue'
+
+    export default {
+        components: {
+            PostPreview
+        },
+        props: {
+            isAdmin: {
+                type: Boolean,
+                default: false
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .post-list {
+        display: flex;
+        padding: 20px;
+        box-sizing: border-box;
+        flex-wrap: wrap;
+        align-items: center;
+        justify-content: center;
+    }
+</style>
---
+++ pages/admin/_postId/index.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="admin-post-page">
+        <section class="update-form">
+            <AdminPostForm :post="loadedPost" />
+        </section>
+    </div>
+</template>
+
+<script>
+    import AdminPostForm from '~/components/Admin/AdminPostForm'
+
+    export default {
+        layout: 'admin',
+        data() {
+            return {
+                loadedPost: {
+                    author: 'Max',
+                    title: 'Post title',
+                    content: 'Some Content',
+                    thumbnailLink: 'https://www.pixelstalk.net/wp-content/uploads/2016/06/HD-High-Tech-Wallpaper.jpg'
+                }
+            }
+        },
+        components: {
+            AdminPostForm
+        }
+    }
+</script>
+
+<style scoped>
+    .update-form {
+        width: 90%;
+        margin: 20px auto;
+    }
+    @media (min-width: 768px) {
+        .update-form {
+            width: 500px;
+        }
+    }
+</style>
